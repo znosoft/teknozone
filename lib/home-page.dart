b7cb69f17252f2bab0b98476a26077bb55d292d0
tugrul orhan <tugrulorhan@hotmail.com>
2023-04-25 17:08:08 +0300

    .

--- lib/home-page.dart
+++ lib/home-page.dart
@@ -2,185 +2,145 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final cardWidth = 200.0;
+  final cardHeight = 80.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: MyColors.grayColor,
         body: Row(
-      children: [
-        Container(
-          margin: const EdgeInsets.all(8),
-          child: Column(
-            children: [
-              Ink(
-                height: 80,
-                width: 80,
-                decoration: ShapeDecoration(
-                    color: Color.fromARGB(255, 95, 93, 93),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)))),
-                child: IconButton(
-                  icon: Image.asset('assets/BT_OFF.jpg'),
-                  iconSize: 10,
-                  onPressed: () {},
-                ),
+          children: [
+            Container(
+              margin: const EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: FittedBox(
+                      child: Row(
+                        children: [
+                          CustomIconButton(
+                            icon: 'assets/BT_OFF.jpg',
+                            onPressed: () {
+                              print("BT_ON");
+                            },
+                          ),
+                          HomePageCustomCard(text: "PPM SET: --.--ppm")
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: FittedBox(
+                      child: Row(
+                        children: [
+                          CustomIconButton(
+                            icon: 'assets/OZONE.jpg',
+                            onPressed: () {
+                              print("OZONE button clicked");
+                            },
+                          ),
+                          HomePageCustomCard(text: "--.--ppm")
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: FittedBox(
+                      child: Row(
+                        children: [
+                          CustomIconButton(
+                            icon: 'assets/TEMP.jpg',
+                            onPressed: () {
+                              print("TEMP button clicked");
+                            },
+                          ),
+                          HomePageCustomCard(text: "--.- 'C")
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: FittedBox(
+                      child: Row(
+                        children: [
+                          CustomIconButton(
+                              icon: 'assets/NEM.jpg',
+                              onPressed: () {
+                                print("TEMP button clicked");
+                              }),
+                          HomePageCustomCard(text: "--.- %Rh")
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: FittedBox(
+                      child: Row(
+                        children: [
+                          CustomIconButton(
+                              icon: 'assets/BT_TIME_OFF.jpg',
+                              onPressed: () {
+                                print("TEMP button clicked");
+                              }),
+                          HomePageCustomCard(text: "--.- 'C")
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(height: 10),
-              Ink(
-                height: 80,
-                width: 80,
-                decoration: ShapeDecoration(
-                    color: Color.fromARGB(255, 95, 93, 93),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)))),
-                child: IconButton(
-                  icon: Image.asset('assets/OZONE.jpg'),
-                  iconSize: 20,
-                  onPressed: () {},
-                ),
-              ),
-              SizedBox(height: 10),
-              Ink(
-                height: 80,
-                width: 80,
-                decoration: ShapeDecoration(
-                    color: Color.fromARGB(255, 95, 93, 93),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)))),
-                child: IconButton(
-                  icon: Image.asset('assets/TEMP.jpg'),
-                  iconSize: 10,
-                  onPressed: () {},
-                ),
-              ),
-              SizedBox(height: 10),
-              Ink(
-                height: 80,
-                width: 80,
-                decoration: ShapeDecoration(
-                    color: Color.fromARGB(255, 95, 93, 93),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)))),
-                child: IconButton(
-                  icon: Image.asset('assets/NEM.jpg'),
-                  iconSize: 10,
-                  onPressed: () {},
-                ),
-              ),
-              SizedBox(height: 10),
-              Ink(
-                height: 80,
-                width: 80,
-                decoration: ShapeDecoration(
-                    color: Color.fromARGB(255, 95, 93, 93),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)))),
-                child: IconButton(
-                  icon: Image.asset('assets/BT_TIME_OFF.jpg'),
-                  iconSize: 10,
-                  onPressed: () {},
-                ),
-              ),
-            ],
+            )
+          ],
+        ));
+  }
+}
+
+class CustomIconButton extends StatelessWidget {
+  final Color grayColor = MyColors.grayColor;
+  CustomIconButton(
+      {Key? key, required this.icon, this.onPressed, this.cardHeight})
+      : super(key: key);
+  final double? cardHeight;
+  final String icon;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    double height = cardHeight ?? 80.0;
+    return SizedBox(
+      height: height,
+      width: height,
+      child: IconButton(
+        icon: Image.asset(icon),
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
+
+class HomePageCustomCard extends StatelessWidget {
+  const HomePageCustomCard({Key? key, required this.text}) : super(key: key);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.black,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      child: SizedBox(
+        width: 200,
+        height: 80,
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(color: Colors.white),
           ),
         ),
-        Container(
-          margin: const EdgeInsets.all(8),
-          child: Column(
-            children: [
-              Container(
-                width: 200,
-                height: 80,
-                color: Colors.black,
-                child: Center(
-                  child: Text(
-                    "PPM SET: --.--ppm",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10.0),
-              Container(
-                width: 200,
-                height: 80,
-                color: Colors.black,
-                child: Center(
-                  child: Text(
-                    "--.--ppm",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10.0),
-              Ink(
-                width: 200,
-                height: 80,
-                padding: EdgeInsets.all(8),
-                decoration: ShapeDecoration(
-                    color: Color.fromARGB(255, 95, 93, 93),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)))),
-                child: Center(
-                  child: Text(
-                    "--.- 'C",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              )
-            ],
-          ),
-        )
-      ],
-    ));
-    // return Scaffold(
-    //     body: Column(children: [
-    //   Row(children: [
-    //     IconButton(
-    //       icon: Image.asset('assets/BT_OFF.jpg'),
-    //       iconSize: 10,
-    //       onPressed: () {},
-    //     ),
-    //     IconButton(
-    //       icon: Image.asset('assets/BT_ON.jpg'),
-    //       iconSize: 10,
-    //       onPressed: () {},
-    //     )
-    //   ]),
-    //   Row(
-    //     children: [
-    //       IconButton(
-    //         icon: Image.asset('assets/OZONE.jpg'),
-    //         iconSize: 10,
-    //         onPressed: () {},
-    //       ),
-    //     ],
-    //   ),
-    //   Row(
-    //     children: [
-    //       IconButton(
-    //         icon: Image.asset('assets/TEMP.jpg'),
-    //         iconSize: 10,
-    //         onPressed: () {},
-    //       ),
-    //     ],
-    //   ),
-    //   Row(
-    //     children: [
-    //       IconButton(
-    //         icon: Image.asset('assets/NEM.jpg'),
-    //         iconSize: 10,
-    //         onPressed: () {},
-    //       ),
-    //     ],
-    //   ),
-    //   Row(
-    //     children: [
-    //       IconButton(
-    //         icon: Image.asset('assets/BT_TIME_OFF.jpg'),
-    //         iconSize: 10,
-    //         onPressed: () {},
-    //       ),
-    //     ],
-    //   ),
-    // ]));
+      ),
+    );
   }
+}
+
+class MyColors {
+  static Color grayColor = Color.fromARGB(255, 95, 93, 93);
 }
--- lib/settings.dart
+++ lib/settings.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home-page.dart';
+
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
   @override
@@ -7,8 +9,134 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPage extends State<SettingsPage> {
+  final Color grayColor = Color.fromARGB(255, 95, 93, 93);
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.black);
+    return Scaffold(
+      backgroundColor: grayColor,
+      body: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+        Container(
+          margin: EdgeInsets.all(8),
+          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+            SettingsBigCard(),
+            CustomIconButton(
+                icon: 'assets/PASS_CHANGE.jpg',
+                onPressed: () {
+                  print("PASS CHANGE button clicked");
+                }),
+            Row(
+              children: [
+                CustomIconButton(
+                    cardHeight: 60,
+                    icon: 'assets/PASS_Check.jpg',
+                    onPressed: () {
+                      print("PASS CHANGE button clicked");
+                    }),
+                CustomIconButton(
+                    cardHeight: 60,
+                    icon: 'assets/BT_SAVE_AP.jpg',
+                    onPressed: () {
+                      print("PASS CHANGE button clicked");
+                    }),
+                CustomIconButton(
+                    cardHeight: 60,
+                    icon: 'assets/BT_SAVE_CD.jpg',
+                    onPressed: () {
+                      print("PASS CHANGE button clicked");
+                    }),
+                CustomIconButton(
+                    cardHeight: 60,
+                    icon: 'assets/BT_SAVE_TIME.jpg',
+                    onPressed: () {
+                      print("PASS CHANGE button clicked");
+                    }),
+                CustomIconButton(
+                    cardHeight: 60,
+                    icon: 'assets/BT_SAVE_DATE.jpg',
+                    onPressed: () {
+                      print("PASS CHANGE button clicked");
+                    }),
+              ],
+            ),
+            Container(
+              color: Colors.yellow,
+              width: 200,
+              height: 120,
+            ),
+            Row(
+              children: [
+                CustomIconButton(
+                    icon: 'assets/BT_SCAN.jpg',
+                    onPressed: () {
+                      print("PASS CHANGE button clicked");
+                    }),
+                CustomIconButton(
+                    icon: 'assets/BT_SAVE.jpg',
+                    onPressed: () {
+                      print("PASS CHANGE button clicked");
+                    }),
+              ],
+            ),
+          ]),
+        )
+      ]),
+    );
+  }
+}
+
+class SettingsBigCard extends StatelessWidget {
+  const SettingsBigCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(8),
+      color: Colors.black,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(0, 0, 8, 0),
+        child: SizedBox(
+          child: Center(
+            child: Row(children: [
+              Image.asset(
+                "assets/BT_TIME_AK.jpg",
+                width: 40,
+              ),
+              SizedBox(width: 20),
+              Column(
+                children: [
+                  Text(
+                    "--:--",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Text(
+                    "--:--",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+              SizedBox(width: 20),
+              Image.asset("assets/BT_TIME_CD.jpg", width: 40),
+              SizedBox(width: 20),
+              Column(
+                children: [
+                  Text(
+                    "--:--",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Text(
+                    "--:--",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+              SizedBox(width: 20),
+            ]),
+          ),
+        ),
+      ),
+    );
   }
 }
