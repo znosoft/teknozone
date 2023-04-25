56aa98daa184e9dec0fbab27b7e8e4dd767c0dd2
tugrul orhan <tugrulorhan@hotmail.com>
2023-04-25 17:22:18 +0300

    .

--- lib/settings.dart
+++ lib/settings.dart
@@ -12,75 +12,75 @@ class _SettingsPage extends State<SettingsPage> {
   final Color grayColor = Color.fromARGB(255, 95, 93, 93);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: grayColor,
-      body: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-        Container(
-          margin: EdgeInsets.all(8),
-          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-            SettingsBigCard(),
-            CustomIconButton(
-                icon: 'assets/PASS_CHANGE.jpg',
-                onPressed: () {
-                  print("PASS CHANGE button clicked");
-                }),
-            Row(
-              children: [
-                CustomIconButton(
-                    cardHeight: 60,
-                    icon: 'assets/PASS_Check.jpg',
-                    onPressed: () {
-                      print("PASS CHANGE button clicked");
-                    }),
-                CustomIconButton(
-                    cardHeight: 60,
-                    icon: 'assets/BT_SAVE_AP.jpg',
-                    onPressed: () {
-                      print("PASS CHANGE button clicked");
-                    }),
-                CustomIconButton(
-                    cardHeight: 60,
-                    icon: 'assets/BT_SAVE_CD.jpg',
-                    onPressed: () {
-                      print("PASS CHANGE button clicked");
-                    }),
-                CustomIconButton(
-                    cardHeight: 60,
-                    icon: 'assets/BT_SAVE_TIME.jpg',
-                    onPressed: () {
-                      print("PASS CHANGE button clicked");
-                    }),
-                CustomIconButton(
-                    cardHeight: 60,
-                    icon: 'assets/BT_SAVE_DATE.jpg',
-                    onPressed: () {
-                      print("PASS CHANGE button clicked");
-                    }),
-              ],
-            ),
-            Container(
-              color: Colors.yellow,
-              width: 200,
-              height: 120,
-            ),
-            Row(
-              children: [
-                CustomIconButton(
-                    icon: 'assets/BT_SCAN.jpg',
-                    onPressed: () {
-                      print("PASS CHANGE button clicked");
-                    }),
-                CustomIconButton(
-                    icon: 'assets/BT_SAVE.jpg',
-                    onPressed: () {
-                      print("PASS CHANGE button clicked");
-                    }),
-              ],
-            ),
-          ]),
-        )
-      ]),
-    );
+    return Flexible(
+        child: ConstrainedBox(
+      constraints: BoxConstraints(maxWidth: 150),
+      child: Container(
+        color: MyColors.grayColor,
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          SettingsBigCard(),
+          CustomIconButton(
+              icon: 'assets/PASS_CHANGE.jpg',
+              onPressed: () {
+                print("PASS CHANGE button clicked");
+              }),
+          Row(
+            children: [
+              CustomIconButton(
+                  cardHeight: 60,
+                  icon: 'assets/PASS_Check.jpg',
+                  onPressed: () {
+                    print("PASS CHANGE button clicked");
+                  }),
+              Card(color: Colors.black, child: SizedBox(width: 20, height: 20)),
+              CustomIconButton(
+                  cardHeight: 60,
+                  icon: 'assets/BT_SAVE_AP.jpg',
+                  onPressed: () {
+                    print("PASS CHANGE button clicked");
+                  }),
+              CustomIconButton(
+                  cardHeight: 60,
+                  icon: 'assets/BT_SAVE_CD.jpg',
+                  onPressed: () {
+                    print("PASS CHANGE button clicked");
+                  }),
+              CustomIconButton(
+                  cardHeight: 60,
+                  icon: 'assets/BT_SAVE_TIME.jpg',
+                  onPressed: () {
+                    print("PASS CHANGE button clicked");
+                  }),
+              CustomIconButton(
+                  cardHeight: 60,
+                  icon: 'assets/BT_SAVE_DATE.jpg',
+                  onPressed: () {
+                    print("PASS CHANGE button clicked");
+                  }),
+            ],
+          ),
+          Container(
+            color: Colors.yellow,
+            width: 200,
+            height: 120,
+          ),
+          Row(
+            children: [
+              CustomIconButton(
+                  icon: 'assets/BT_SCAN.jpg',
+                  onPressed: () {
+                    print("PASS CHANGE button clicked");
+                  }),
+              CustomIconButton(
+                  icon: 'assets/BT_SAVE.jpg',
+                  onPressed: () {
+                    print("PASS CHANGE button clicked");
+                  }),
+            ],
+          ),
+        ]),
+      ),
+    ));
   }
 }
 
