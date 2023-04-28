06b595335d899117652f18c2b6fc800cdccef785
znosoft <tugrulorhan@znosoft.net>
2023-04-28 13:17:40 +0300

    .

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:teknozone/main.dart';
+import 'package:teknozone/_main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
